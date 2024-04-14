e78c93f440112491b40b7d0e8f1a4a336cf3cc56
NaoP <naop18@yahoo.fr>
2024-04-14 21:22:30 +0100

    Fin about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,19 @@
 export default function About() {
     return <>
-        <section className="about-container">
+        <section id="#About" className="about-container">
             <h2>
                 A propos de moi
             </h2>
             <p>
-                Je suis Nao, un développeur web passionné par la technologie et l'apprentissage continue.
-                Avec un background en commerce et marketing à l'IDRAC et Certifié par HEC en business model
-                development, j'ai une solide expérience dans le e-commerce en France et à l'international.
-                Mon portfolio reflète mon engagement à combiner expertise technique et stratégique pour créer
-                des solutions web innovantes et orientées vers les résultats.
+                Je suis Nao, passionné par la technologie et l'apprentissage continue.
+                Avec un background en commerce et marketing à l'IDRAC et Certifié par <span>HEC</span> en business model
+                development, j'ai une solide expérience dans le <span>e-commerce</span> en France et à l'international.
+                Gouvernement, événementiel, médical... j'ai pu m'aventurer à travers plusieurs domaines et partager des
+                visions differentes. <br /> Mon <span>portfolio</span> reflète mon engagement à combiner expertise technique et stratégique
+                pour créer des solutions web innovantes et orientées vers les résultats.
             </p>
         </section>
     </>
 } 
+
+/** voir pour mettre la balise que ca descende correctement soit id soit Navlinks c mieux */
