dfd67ebf4cdea3aa6e1d282752fda3e7f6b93e7d
NaoP <naop18@yahoo.fr>
2024-04-15 00:02:14 +0100

    Création, import CardService et props

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,9 +1,15 @@
-import card from "./Card";
+import CardService from "./Card";
+import codeImg from "../assets/code-solid.svg";
 
 export default function Service() {
-    return<>
-    <section>
-
-    </section>
+    return <>
+        <section>
+            <CardService
+                minia={codeImg}
+                imgAlt="Vector balise de code"
+                title="Développement Frontend"
+                content="fdksgqfkj gdfk gdqskjfh qds fhsdgkfqj dgsf dskjfh 
+                sdjkhfds qkjfhv dsjkhfv sdkfjh sq" />
+        </section>
     </>
 }
