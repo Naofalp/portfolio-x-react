89aca53f7f5c8bb506027bf0f88f6cc994c16556
NaoP <naop18@yahoo.fr>
2024-04-14 22:31:09 +0100

    fin about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,15 +5,14 @@ export default function About() {
                 A propos de moi
             </h2>
             <p>
-                Je suis Nao, passionné par la technologie et l'apprentissage continue.
-                Avec un background en commerce et marketing à l'IDRAC et Certifié par <span>HEC</span> en business model
-                development, j'ai une solide expérience dans le <span>e-commerce</span> en France et à l'international.
-                Gouvernement, événementiel, médical... j'ai pu m'aventurer à travers plusieurs domaines et partager des
-                visions differentes. <br /> Mon <span>portfolio</span> reflète mon engagement à combiner expertise technique et stratégique
-                pour créer des solutions web innovantes et orientées vers les résultats.
+                Je suis Nao, passionné par la technologie et l'apprentissage continue.<br/>
+                Diplomé dans plusieurs domaines et ayant travaillé dans le monde entier, j'ai
+                une solide expérience dans le e-commerce. Gouvernement, événementiel, médical...
+                j'ai pu m'aventurer à travers plusieurs secteurs et en tirer bénéfice dans ma vision 
+                des choses.
             </p>
         </section>
     </>
-} 
+}
 
 /** voir pour mettre la balise que ca descende correctement soit id soit Navlinks c mieux */
