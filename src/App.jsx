8bfe01aad6f9327eb64a30f17ef5c1ff7a324a51
NaoP <naop18@yahoo.fr>
2024-04-14 20:02:19 +0100

    Création About a propos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from "./components/Header";
 import Banner from "./components/Banner";
+import About from "./components/About";
 
 function App() {
   return <>
     <Header />
     <main>
       <Banner />
+      <About />
     </main>
   </>
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+export default function About() {
+    return <>
+        <section className="about-container">
+            <h2>
+                A propos de moi
+            </h2>
+            <p>
+                Je suis Nao, un développeur web passionné par la technologie et l'apprentissage continue.
+                Avec un background en commerce et marketing à l'IDRAC et Certifié par HEC en business model
+                development, j'ai une solide expérience dans le e-commerce en France et à l'international.
+                Mon portfolio reflète mon engagement à combiner expertise technique et stratégique pour créer
+                des solutions web innovantes et orientées vers les résultats.
+            </p>
+        </section>
+    </>
+} 
