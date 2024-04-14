de4ba57c72b59826565931ea5ea34be8bbe9e4a2
NaoP <naop18@yahoo.fr>
2024-04-15 00:01:37 +0100

    ajout Card et ses prop

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,9 @@
-export default function Card() {
+export default function CardService({minia, imgAlt, title, content}) {
     return<>
-    
+    <article className="card_article">
+        <img src={minia} alt={imgAlt} className="card_minia"/>
+        <h3>{title}</h3>
+        <p className="card_content">{content}</p>
+    </article>
     </>
 }
