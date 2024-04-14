30d70e88e405c6edcd0355f486f49e856a3b1d9d
NaoP <naop18@yahoo.fr>
2024-04-15 00:02:33 +0100

    import service

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from "./components/Header";
 import Banner from "./components/Banner";
 import About from "./components/About";
+import Service from './components/Service';
 
 function App() {
   return <>
@@ -9,6 +10,7 @@ function App() {
     <main>
       <Banner />
       <About />
+      <Service />
     </main>
   </>
 }
