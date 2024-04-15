59e8fec4deded0663eccae7a2d82ce184895664a
NaoP <naop18@yahoo.fr>
2024-04-15 17:55:34 +0100

    travail grid

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -3,13 +3,25 @@ import codeImg from "../assets/code-solid.svg";
 
 export default function Service() {
     return <>
-        <section>
+        <section className="card_container">
             <CardService
                 minia={codeImg}
                 imgAlt="Vector balise de code"
                 title="Développement Frontend"
-                content="fdksgqfkj gdfk gdqskjfh qds fhsdgkfqj dgsf dskjfh 
-                sdjkhfds qkjfhv dsjkhfv sdkfjh sq" />
+                content="Your website should look good and behave correctly from any device, 
+                of any size, every single time. We can help!" />
+                <CardService
+                minia={codeImg}
+                imgAlt="Vector balise de code"
+                title="Web marketing"
+                content="Your website should look good and behave correctly from any device, 
+                of any size, every single time. We can help!" />
+                <CardService
+                minia={codeImg}
+                imgAlt="Vector balise de code"
+                title="Stratégie"
+                content="Your website should look good and behave correctly from any device, 
+                of any size, every single time. We can help!" />
         </section>
     </>
 }
