5bf4e392831c97bba33d95d2604f42740ff47215
NaoP <naop18@yahoo.fr>
2024-05-06 18:07:56 +0200

    Création Tools

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Banner from "./components/Banner";
 import About from "./components/About";
 import Service from './components/Service';
+import Tools from './components/Tools';
 
 function App() {
   return <>
