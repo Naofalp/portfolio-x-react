1bc94f0afaf3f198bd7f8a3d8d7314eadc7b21f5
NaoP <naop18@yahoo.fr>
2024-05-15 17:44:44 +0200

    Responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,7 @@
 export default function Nav() {
     return <>
         <nav className="nav-header">
-            <a href="#About" className="nav-header_link" >À propos</a>
+            <a href="#About" className="nav-header_link disapear" >À propos</a>
             <a href="#Competences" className="nav-header_link" >Compétences</a>
             <a href="#Portfolio" className="nav-header_link" >Portfolio</a>
             <a href="#Contact" className="nav-header_link" >Contact</a>
