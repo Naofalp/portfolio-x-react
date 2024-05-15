9e466cc1a8b99cfdb0af9b58aa7c5ddb1ac210e3
NaoP <naop18@yahoo.fr>
2024-05-15 14:01:04 +0200

    Ajout button

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -12,23 +12,23 @@ export default function Works() {
             <div className="cardPortfolio_container">
                 <div className="cardPortfolio_article">
                     <figure className='cardPortfolio_figure'>
-                        <img src={nina} alt="Debuggage et Optimisation site nina carducci"/>
+                        <img src={nina} alt="Debuggage et Optimisation site nina carducci" />
                         <figcaption>
                             <h3> Debuggage et Optimisation </h3>
-                            <a href="https://naofalp.github.io/Nina-carducci/" target="_blank" rel="noopener noreferrer">
+                            <button className="button-55" role="button" onClick={()=>window.open('https://naofalp.github.io/Nina-carducci/', '_blank', 'noopener noreferrer')}>
                                 Visiter
-                            </a>
+                            </button>
                         </figcaption>
                     </figure>
                 </div>
                 <div className="cardPortfolio_article">
                     <figure className='cardPortfolio_figure'>
-                        <img src={kasa} alt="Site de location immobilière Kasa sous app REACT router"/>
+                        <img src={kasa} alt="Site de location immobilière Kasa sous app REACT router" />
                         <figcaption>
                             <h3> Site de location immobilière Kasa </h3>
-                            <a href="https://github.com/Naofalp/kasaREACT" target="_blank" rel="noopener noreferrer">
+                            <button className="button-55" role="button" onClick={()=>window.open('https://github.com/Naofalp/kasaREACT', '_blank', 'noopener noreferrer')}>
                                 Visiter
-                            </a>
+                            </button>
                         </figcaption>
                     </figure>
                 </div>
@@ -37,9 +37,9 @@ export default function Works() {
                         <img src={kanban} alt="Tableau Kanban Méthode agile sur Trello" />
                         <figcaption>
                             <h3> Planification de sprints et methode agile </h3>
-                            <a href="https://trello.com/b/iT9PivtB/menumakers-by-qwenta" target="_blank" rel="noopener noreferrer">
+                            <button className="button-55" role="button" onClick={()=>window.open('https://trello.com/b/iT9PivtB/menumakers-by-qwenta', '_blank', 'noopener noreferrer')}>
                                 Visiter
-                            </a>
+                            </button>
                         </figcaption>
                     </figure>
                 </div>
@@ -48,20 +48,20 @@ export default function Works() {
                         <img src={booki} />
                         <figcaption>
                             <h3> Maquette de site de notation d'hébergements </h3>
-                            <a href="https://naofalp.github.io/Booki/" target="_blank" rel="noopener noreferrer">
+                            <button className="button-55" role="button" onClick={()=>window.open('https://naofalp.github.io/Booki/', '_blank', 'noopener noreferrer')}>
                                 Visiter
-                            </a>
+                            </button>
                         </figcaption>
                     </figure>
                 </div>
                 <div className="cardPortfolio_article">
                     <figure className='cardPortfolio_figure'>
-                        <img src={backend} alt="Site de notation de livre MongoDB Express API Backend"/>
+                        <img src={backend} alt="Site de notation de livre MongoDB Express API Backend" />
                         <figcaption>
                             <h3> Réalisation du back-end d'un site de notation de livre </h3>
-                            <a href="https://github.com/Naofalp/Projet-7-Back-End/tree/master" target="_blank" rel="noopener noreferrer">
+                            <button className="button-55" role="button" onClick={()=>window.open('https://github.com/Naofalp/Projet-7-Back-End/tree/master', '_blank', 'noopener noreferrer')}>
                                 Visiter
-                            </a>
+                            </button>
                         </figcaption>
                     </figure>
                 </div>
