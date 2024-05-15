1c68af9101c3b28b0f9e689439b1ebf50785a09d
NaoP <naop18@yahoo.fr>
2024-05-15 16:21:51 +0200

    fin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,19 @@
+import Github from '../assets/Logos/github.svg'
+
 export default function Footer() {
-    return<>
+    return <>
+        <section className="footer_container">
+            <figure>
+                <span>
+                    NPromotion
+                </span>
+            </figure>
+            <a href="https://github.com/Naofalp" target="_blank">
+                <img className="githubLogo_footer" src={Github} alt="GitHub logo" />
+            </a>
+            <p>
+                Réalisé avec React & SASS.
+            </p>
+        </section>
     </>
 }
