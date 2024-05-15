29836fe48ffe23d855d32cbe07b3584028e78006
NaoP <naop18@yahoo.fr>
2024-05-15 15:04:08 +0200

    fin sauf margin negatif

---
+++ src/components/Contact.jsx
@@ -0,0 +1,16 @@
+export default function Contact() {
+    return <>
+        <section id="contact" className="contact_container">
+            <h2 className="contact_title">Démarrer un projet</h2>
+            <p className="contact_texte">Vous êtes interessé pour travailler ensemble ?
+                Je suis toujours ouvert à de nouvelles opportunités.
+            </p>
+            <div className="buttonContact_container">
+            <button className="buttonContact" onClick={() => window.location.href = 'mailto:naofal.pereira@yahoo.fr'}>
+                C'est parti 😁
+            </button>
+            </div>
+
+        </section>
+    </>
+}
